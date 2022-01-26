0c2a860ef0f32aa30f0dfcceff5ce057f13d73c8
Hao Ren <haoren.2906@gmail.com>
2022-01-27 01:45:53 +1100

    Split codes with their datasets, add some testing results.

--- codes/utils.py
+++ codes/utils.py
@@ -1,8 +1,14 @@
+from os.path import dirname, join
 from StageTwo import *
 from DataLoading import *
 
-path1 = '../datasets/Alaska/Kaktovi.edges.csv'
-network1, g1, nodesLayers1 = loadAlaskaDataIntoNetwork(path1)
+script_dir = dirname(dirname(__file__))
+
+################################################################################
+
+path1 = 'datasets/Alaska/Kaktovi.edges.csv'
+abs_path1 = join(script_dir, path1)
+network1, g1, nodesLayers1 = loadAlaskaDataIntoNetwork(abs_path1)
 
 lam1 = 0.5
 sig1 = 0.5
@@ -12,8 +18,11 @@ for layer1 in range(g1):
     xbar1[layer1] = np.ones(nodesLayers1[layer1])
 Alaska = stage2(network1, nodesLayers1, lam1, sig1, xbar1)
 
-path2 = '../datasets/EUAir/EUAirTransportation_multiplex.csv'
-network2, g2, nodesLayers2 = loadEUAirTransportDataIntoNetwork(path2)
+################################################################################
+
+path2 = 'datasets/EUAir/EUAirTransportation_multiplex.csv'
+abs_path2 = join(script_dir, path2)
+network2, g2, nodesLayers2 = loadEUAirTransportDataIntoNetwork(abs_path2)
 
 lam2 = 0.5
 sig2 = 0.5
@@ -22,3 +31,5 @@ for layer2 in range(g2):
     # xbar2[layer2] = np.random.rand(nodesLayers2[layer2])
     xbar2[layer2] = np.ones(nodesLayers2[layer2])
 EUAir = stage2(network2, nodesLayers2, lam2, sig2, xbar2)
+
+################################################################################
