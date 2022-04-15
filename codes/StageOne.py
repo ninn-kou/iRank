7e7b4ceb7baab9d00b498e294c835dbffb23c034
Hao Ren <haoren.2906@gmail.com>
2022-04-16 06:56:56 +1000

    [Fix] Stage 1 code debugged for testing datasets.
    * Improve the stability by initialisation X and Z as a constant.
    * Setting the iteration times by given related error value.
    * Clear unnecessary codes.

--- codes/StageOne.py
+++ codes/StageOne.py
@@ -1,14 +1,26 @@
 import numpy as np
 
-# TODO: Iterations not working.
 
 def stage1(Network, Size, a, s, gamma, delta=0.85):
     """
-    H:
-        (1 * g) ndarray, number of nodes in each layer.
+    Network:
+        (g * n * n) ndarray, g layers adjacency matrices.
+    Size:
+        (g) ndarray, number of nodes in each layer.
+    a: the influence of a layer is
+        (a = 1) proportional to W[layer].
+        (a = 0) normalized with respect to W[layer].
+    s: layers have larger influence if they include
+        (s = 1)  more central nodes.
+        (s = -1) fewer highly influential nodes.
+    gamma:
+        (gamma > 1) enhance the contribution of low centrality nodes for Z calculation.
+        (gamma < 1) suppress the contribution of low centrality nodes for Z calculation.
+    delta:
+        (delta = 0.85) as usual in the context of the PageRank algorithms.
     """
 
-    v_quadratic_error = 0.0001
+    error = 1e-6
 
     g = len(Size)
     n = Size[0]
@@ -23,49 +35,46 @@ def stage1(Network, Size, a, s, gamma, delta=0.85):
         B[layer] = np.sum(A[layer], axis=0) / W[layer]
 
     # Centrality of layer initialized as 0.
-    Z = np.random.uniform(0, 1, g)
+    # Z = np.random.uniform(0, 1, g)
+    # Set the initial Z value as a constant to avoid random effects when calculation
+    # from (gamma == 0.1) to (gamma == 3.0).
+    Z = np.full(g, 0.5)
 
     G = np.zeros((n, n), dtype=float)
     for layer in range(g):
         G += A[layer] * Z[layer]
 
     # Centrality of node initialized as 0.
-    X = np.random.uniform(0, 1, n)
+    # X = np.random.uniform(0, 1, n)
+    # Set the initial X value as a constant to avoid random effects when calculation
+    # from (gamma == 0.1) to (gamma == 3.0).
+    X = np.full(n, 0.5)
 
     # V_i initialized as 0.
     V = np.zeros(n, dtype=float)
     # V_i = \sum^{g}_{j=1} [G_{ij} + G_{ji}
     V = np.sum(G, axis=1) + np.sum(G, axis=0)
-    # Apply θ: Heaviside step function to array V.
+    # Apply theta (Heaviside step function) to array V.
     V = np.where(V <= 0, 0, V)
 
-    # 1st Iteration by using random values of X and Z.
-
-    beta = np.sum((1 - delta * (np.sum(G, axis=0) > 1)) * X) / np.sum(V)
-
-    # We need another variable to store the value of X at the beginning of
-    # each iteration because X value only changes when moving to the next
-    # iteration but is not in progress.
-    X_constant = X
-    for layer in range(g):
-        X += B[layer] * X_constant / np.sum(B[layer])
-    X += V * beta
-    X /= np.sum(X)
-
-    for layer in range(g):
-        Z[layer] = (W[layer] ** a) * (np.sum(B[layer] * (X ** (s * gamma))) ** s)
-    Z /= np.sum(Z)
-
-    # Iterations.
-    while np.linalg.norm(X - X_constant, 2) > v_quadratic_error * np.linalg.norm(X):
+    # Iterations stop when related error is less than setting value.
+    while True:
         beta = np.sum((1 - delta * (np.sum(G, axis=0) > 1)) * X) / np.sum(V)
+
         X_constant = X
+        X = np.zeros(n, dtype=float)
         for layer in range(g):
             X += B[layer] * X_constant / np.sum(B[layer])
         X += V * beta
         X /= np.sum(X)
+
+        Z = np.zeros(g, dtype=float)
         for layer in range(g):
             Z[layer] = (W[layer] ** a) * (np.sum(B[layer] * (X ** (s * gamma))) ** s)
         Z /= np.sum(Z)
+
+        # Stopping condition.
+        if np.average(np.absolute(X - X_constant)) < error:
+            break
 
     return X, Z
