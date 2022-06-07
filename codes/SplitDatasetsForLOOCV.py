841ed9c53bcb94745af7cf5c7b6428733f91a89f
Hao Ren <haoren.2906@gmail.com>
2022-06-07 16:17:10 +1000

    Update SplitDatasetsForLOOCV.py

--- codes/SplitDatasetsForLOOCV.py
+++ codes/SplitDatasetsForLOOCV.py
@@ -27,6 +27,11 @@ path = "EUAir_Transportation_multiplex.csv"
 from_node = 1
 to_node = 2
 
+# 3. `nodes_num` is the number of nodes in the original dataset.
+# It would be better to input manually, because if the last node has been
+# removed in the LOOCV process, the algorithm returning size would be wrong.
+nodes_num = 450
+
 ################################################################################
 
 folder = "LOOCV_subdatasets_" + path.split("_")[0]
@@ -36,9 +41,9 @@ if not os.path.exists(folder):
 dataset = pd.read_csv(path, header=None)
 
 all_nodes = list(set(list(dataset[from_node]) + list(dataset[to_node])))
-max_node = max(all_nodes)
+# nodes_num = max(all_nodes)
 
-for leave_node in range(1, max_node + 1):
+for leave_node in range(1, nodes_num + 1):
     if leave_node in all_nodes:
         sub_dataset = dataset[
             (dataset[from_node] != leave_node) & (dataset[to_node] != leave_node)
@@ -47,3 +52,4 @@ for leave_node in range(1, max_node + 1):
         sub_dataset = dataset
     new_path = folder + "/%s_%s.csv" % (path.split("_")[0], str(leave_node))
     sub_dataset.to_csv(new_path, index=None, header=None)
+
