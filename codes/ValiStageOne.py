950bb076e01929089541e6a9fd80c2a323fb4ef1
Hao Ren <haoren.2906@gmail.com>
2022-07-30 18:37:04 +1000

    Adding EUAir dataset experiments.

--- codes/ValiStageOne.py
+++ codes/ValiStageOne.py
@@ -18,6 +18,38 @@ def s1_betweenness_centrality(Network, Size):
     return nx_betweenness_centrality
 
 
+def s1_closeness_centrality(Network, Size):
+    """Calculate the closeness centrality of each node in the network.
+    Network:
+        (g * n * n) ndarray, g layers supra adjacency matrices.
+    Size:
+        (g) ndarray, number of nodes in each layer.
+    """
+    nx_closeness_centrality = np.zeros(Size[0], dtype=float)
+    for layer in range(len(Size)):
+        G = nx.from_numpy_array(Network[layer][layer])
+        T = nx.closeness_centrality(G)
+        for node in range(Size[0]):
+            nx_closeness_centrality[node] += T[node]
+    return nx_closeness_centrality
+
+
+def s1_degree_centrality(Network, Size):
+    """Calculate the degree centrality of each node in the network.
+    Network:
+        (g * n * n) ndarray, g layers supra adjacency matrices.
+    Size:
+        (g) ndarray, number of nodes in each layer.
+    """
+    nx_degree_centrality = np.zeros(Size[0], dtype=float)
+    for layer in range(len(Size)):
+        G = nx.from_numpy_array(Network[layer][layer])
+        T = nx.degree_centrality(G)
+        for node in range(Size[0]):
+            nx_degree_centrality[node] += T[node]
+    return nx_degree_centrality
+
+
 def s1_eigenvector_centrality(Network, Size):
     """Calculate the eigenvector centrality of each node in the network.
     Network:
