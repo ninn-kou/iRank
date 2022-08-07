559f5ab4a1c63d88acb8cc30ab31c128a5067d86
Hao Ren <haoren.2906@gmail.com>
2022-08-07 10:06:44 +1000

    Add two datasets for application on multiplex networks.

---
+++ datasets/CElegans/DataSplitLOOCV.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5290998b27a439b42ab8459c482834265fc76f963d9e55fa40db254b5c255252
+size 1950
---
+++ datasets/Drosophila/DataSplitLOOCV.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2274cf87ea6f85aaf0105e2839794ebb6bcd1fd2ab1bc29d0d075e08a9fb3da8
+size 1950
